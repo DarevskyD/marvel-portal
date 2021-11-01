d863a489a52e7aff55960c5246b46c9afa3016ad
Dmitry Darevsky <darevsky.dmitry@gmail.com>
2021-11-01 16:31:42 +0300

    Work with structure and design of components

--- src/components/socialFollow/SocialFollow.js
+++ src/components/socialFollow/SocialFollow.js
@@ -5,41 +5,41 @@ import {
   FaInstagram,
   FaTwitter,
 } from "react-icons/fa";
-import '../../style/variables.scss';
-import "./socialFollow.scss"
+import "../../style/variables.scss";
+import "./socialFollow.scss";
 
 const SocialFollow = () => {
-  return (    
-      <div class="app__footer-navigation">
-        <h4 class="app__footer-navigation-title">FOLLOW MARVEL</h4>
-        <ul class="app__footer-navigation-links">
-          <li>
-            <a href="#">
-              <FaFacebookSquare className="link"/>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaTwitter className="link"/>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaYoutube className="link"/>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaInstagram className="link"/>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaPinterest className="link"/>
-            </a>
-          </li>
-        </ul>
-      </div>    
+  return (
+    <div className="app__footer-navigation">
+      <h4 className="app__footer-navigation-title">FOLLOW MARVEL</h4>
+      <ul className="app__footer-navigation-links">
+        <li>
+          <a href="#">
+            <FaFacebookSquare className="link" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <FaTwitter className="link" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <FaYoutube className="link" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <FaInstagram className="link" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <FaPinterest className="link" />
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
