72214f59263c782d8c46bb9d4c7dce9bbabddb5f
Dmitry Darevsky <darevsky.dmitry@gmail.com>
2022-01-20 18:54:30 +0300

    Fix small bugs

--- src/components/socialFollow/SocialFollow.js
+++ src/components/socialFollow/SocialFollow.js
@@ -14,27 +14,43 @@ const SocialFollow = () => {
       <h4 className="app__footer-navigation-title">FOLLOW MARVEL</h4>
       <ul className="app__footer-navigation-links">
         <li>
-          <a href="#">
+          <a
+            href="https://www.facebook.com/Marvel/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaFacebookSquare className="link" />
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="https://twitter.com/marvel" target="_blank" rel="noreferrer">
             <FaTwitter className="link" />
           </a>
         </li>
         <li>
-          <a href="#">
+          <a
+            href="https://www.youtube.com/marvel"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaYoutube className="link" />
           </a>
         </li>
         <li>
-          <a href="#">
+          <a
+            href="https://www.instagram.com/marvel/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaInstagram className="link" />
           </a>
         </li>
         <li>
-          <a href="#">
+          <a
+            href="https://www.pinterest.com/marvelofficial/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <FaPinterest className="link" />
           </a>
         </li>
